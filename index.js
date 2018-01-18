8ee0007446aa887c2a839b493f96f1025251c0db
Amanda Wehrman <arwehrman@gmail.com>
2018-01-18 01:55:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 function selectDifferentDrivers(drivers, func){
-  return func(driver);
+  return func(drivers);
 }
